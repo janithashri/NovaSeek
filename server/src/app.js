5e4971e518d8006891eeef034b3574fce5c3ed92
Admin <janithashri@gmail.com>
2025-03-17 12:14:38 +0530

    Launch  NOVASEEK

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,7 @@ const cors = require('cors');
 const path = require('path');
 const express = require('express');
 const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
-
+const launchesRouter = require('./routes/launches/launches.router'); // Ensure the correct file path
 const app = express();
 const morgan = require('morgan');
 
@@ -14,9 +13,8 @@ app.use(cors({
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
 app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter); // Using correct launchesRouter
+app.use('/launches', launchesRouter); // Make sure this matches the router you're using
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
